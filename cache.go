df2e2a09ed2a19fb3c5d7c638b57e3479eb3ee30
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-05-26 00:00:50 +0400

    solution: cache implementation

--- cache.go
+++ cache.go
@@ -2,22 +2,63 @@ package cache
 
 import "time"
 
+type keyPair struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	keyPairs map[string]keyPair
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (r *Cache) Get(key string) (string, bool) {
+	cache, ok := r.keyPairs[key]
+	currentTime := time.Now()
 
+	if currentTime.After(cache.deadline) {
+		delete(r.keyPairs, key)
+
+		return "", false
+	}
+
+	if !ok {
+		return "", false
+	}
+
+	return cache.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (r *Cache) Put(key, value string) {
+	r.keyPairs[key] = keyPair{
+		value:    value,
+		deadline: time.Time{},
+	}
 }
 
-func (receiver) Keys() []string {
+func (r *Cache) Keys() []string {
+	keys := make([]string, 0, len(r.keyPairs))
+	currentTime := time.Now()
+
+	for key, value := range r.keyPairs {
+		if currentTime.After(value.deadline) {
+			delete(r.keyPairs, key)
+
+			continue
+		}
+
+		keys = append(keys, key)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (r *Cache) PutTill(key, value string, deadline time.Time) {
+	r.keyPairs[key] = keyPair{
+		value:    value,
+		deadline: deadline,
+	}
 }
